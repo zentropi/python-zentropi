f04e2427667e213ebc4dcb098c60c6bcc7341b85
Harshad Sharma <harshad@sharma.io>
2020-03-08 17:02:58 +0530

    Implemented methods for JSON and dict conversion of Frame

--- src/zentropi/frame.py
+++ src/zentropi/frame.py
@@ -1,3 +1,4 @@
+import json
 from enum import IntEnum
 from typing import Dict
 from typing import Optional
@@ -16,17 +17,19 @@ class Frame(object):
     """Frame contains the information that is sent over wire
     between instances and agents.
     """
+    __slots__ = ('_name', '_kind', '_uuid', '_meta', '_data')
+
     def __init__(self, name: str,
-                 uuid: Optional[str] = None,
                  kind: Optional[int] = None,
+                 uuid: Optional[str] = None,
                  data: Optional[Dict] = None,
                  meta: Optional[Dict] = None):
         """
         Frame constructor.
         """
         self._name = name
-        self._uuid = uuid or uuid4().hex
         self._kind = kind or Kind.EVENT
+        self._uuid = uuid or uuid4().hex
         self._data = data
         self._meta = meta
 
@@ -53,3 +56,34 @@ class Frame(object):
         if self._meta is None:
             self._meta = {}
         return self._meta
+
+    def to_dict(self) -> dict:
+        return deflate_dict({
+            'name': str(self.name),
+            'kind': int(self.kind),
+            'uuid': str(self.uuid),
+            'data': dict(self.data) if self.data else {},
+            'meta': dict(self.meta) if self.meta else {},
+        })
+
+    @staticmethod
+    def from_dict(frame_as_dict):
+        return Frame(**frame_as_dict)
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    @staticmethod
+    def from_json(frame_as_json):
+        return Frame.from_dict(json.loads(frame_as_json))
+
+    def reply(self, name='', data=None, meta=None):
+        if isinstance(meta, dict):
+            meta.update({'reply_to': self.uuid})
+        else:
+            meta = {'reply_to': self.uuid}
+        return Frame(name=name or self.name, data=data, meta=meta)
+
+
+def deflate_dict(frame_as_dict):
+    return {k: v for k, v in frame_as_dict.items() if v}
--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1,9 +1,11 @@
+import json
 from uuid import uuid4
 
 import pytest
 
 from zentropi import Frame
 from zentropi import Kind
+from zentropi.frame import deflate_dict
 
 
 @pytest.mark.xfail(raises=TypeError)
@@ -45,3 +47,74 @@ def test_frame_creation_with_meta():
     f = Frame('test-frame', meta=meta)
     assert f.name == 'test-frame'
     assert f.meta == meta
+
+
+def test_frame_to_dict():
+    uuid = uuid4().hex
+    frame_as_dict = {'name': 'test-frame', 'uuid': uuid, 'kind': Kind.EVENT}
+    f = Frame('test-frame', uuid=uuid)
+    fdict = f.to_dict()
+    assert fdict == frame_as_dict
+
+
+def test_frame_from_dict():
+    uuid = uuid4().hex
+    frame_as_dict = {'name': 'test-frame', 'uuid': uuid, 'kind': Kind.EVENT}
+    f = Frame.from_dict(frame_as_dict)
+    assert f.name == 'test-frame'
+    assert f.uuid == uuid
+    assert f.kind == Kind.EVENT
+
+
+def test_frame_to_json():
+    uuid = uuid4().hex
+    frame_as_dict = {'name': 'test-frame', 'uuid': uuid, 'kind': Kind.EVENT}
+    f = Frame('test-frame', uuid=uuid)
+    fjson = f.to_json()
+    fdict = json.loads(fjson)
+    assert fdict == frame_as_dict
+
+
+def test_frame_from_json():
+    uuid = uuid4().hex
+    frame_as_dict = {'name': 'test-frame', 'uuid': uuid, 'kind': Kind.EVENT}
+    frame_as_json = json.dumps(frame_as_dict)
+    f = Frame.from_json(frame_as_json)
+    assert f.name == 'test-frame'
+    assert f.uuid == uuid
+    assert f.kind == Kind.EVENT
+
+
+def test_frame_reply():
+    f = Frame('test-frame')
+    freply = f.reply()
+    assert freply.meta.get('reply_to') == f.uuid
+
+
+def test_frame_reply_with_name():
+    f = Frame('test-frame')
+    freply = f.reply('test-reply')
+    assert freply.name == 'test-reply'
+    assert freply.meta.get('reply_to') == f.uuid
+
+
+def test_frame_reply_with_data():
+    data = {'test': 'item'}
+    f = Frame('test-frame')
+    freply = f.reply(data=data)
+    assert freply.meta.get('reply_to') == f.uuid
+    assert freply.data.get('test') == 'item'
+
+
+def test_frame_reply_with_meta():
+    meta = {'test': 'item'}
+    f = Frame('test-frame')
+    freply = f.reply(meta=meta)
+    assert freply.meta.get('reply_to') == f.uuid
+    assert freply.meta.get('test') == 'item'
+
+
+def test_delate_dict():
+    inflated_dict = {'test': 'item', 'empty': ''}
+    deflated_dict = deflate_dict(inflated_dict)
+    assert deflated_dict == {'test': 'item'}
