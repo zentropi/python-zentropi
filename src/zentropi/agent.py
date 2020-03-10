d51d5c066b5c9af0cdeeb4ca6599a6df26b1cd4c
Harshad Sharma <harshad@sharma.io>
2020-03-10 15:57:53 +0530

    Add join() and leave(), add '*' for command and event handlers and unique names for frame handlers in _spawned_tasks

--- src/zentropi/agent.py
+++ src/zentropi/agent.py
@@ -1,5 +1,8 @@
 import asyncio
 import logging
+import random
+import string
+import traceback
 from asyncio import AbstractEventLoop
 from asyncio import CancelledError
 from asyncio import Event
@@ -16,6 +19,7 @@ from .kind import Kind
 from .transport.base import BaseTransport
 from .transport.queue import QueueTransport
 from .transport.websocket import WebsocketTransport
+from logging import warning
 
 logger = logging.getLogger(__name__)
 
@@ -52,7 +56,7 @@ class Agent(object):
 
     async def _start_interval_tasks(self):
         for int_name, int_task in self._handlers_interval.items():
-            self.spawn(int_task(), name=int_name)
+            self.spawn(int_task(), name=f'interval-task-{int_name}')
 
     async def _run_startup_handler(self):
         if 'startup' in self._handlers_event:
@@ -102,7 +106,11 @@ class Agent(object):
                 self.stop()
             finally:
                 del self._spawned_tasks[task_id]
-        task_id = name or uuid4().hex
+        random_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
+        if name in ['recv-loop'] or name.startswith('interval-task'):
+            task_id = name
+        else:
+            task_id = name + '-' + random_id
         task = self._loop.create_task(watch(task_id, coro))
         self._spawned_tasks.update({task_id: task})
         return task_id, task
@@ -177,12 +185,24 @@ class Agent(object):
             logger.debug('Connection was closed.')
 
     async def _frame_handler(self, frame) -> None:
-        if frame.kind == Kind.COMMAND:
-            handler = self._handlers_command[frame.name]
-        elif frame.kind == Kind.EVENT:        
-            handler = self._handlers_event[frame.name]
+        kind = frame.kind
+        name = frame.name
+        if kind == Kind.COMMAND:
+            if name in self._handlers_command:
+                handler = self._handlers_command[name]
+            elif '*' in self._handlers_command:
+                handler = self._handlers_command['*']
+            else:
+                return
+        elif kind == Kind.EVENT:        
+            if name in self._handlers_event:
+                handler = self._handlers_event[name]
+            elif '*' in self._handlers_event:
+                handler = self._handlers_event['*']
+            else:
+                return
         else:
-            raise KeyError(f'Unknown kind {frame.kind} in {frame.name}')
+            raise KeyError(f'Unknown kind {kind} in {name}')
         self.spawn(handler(frame), name=f'frame-handler-{frame.name}')
 
     def _siginfo_handler(self, *args) -> None:
@@ -198,3 +218,21 @@ class Agent(object):
     def _sigterm_handler(self, *args) -> None:
         logger.warning('Received termination signal.')
         self.shutdown_event.set()
+
+    async def join(self, spaces):
+        if isinstance(spaces, str):
+            spaces = [spaces]
+        else:
+            spaces = list(spaces)
+        logger.warning(f'Joining spaces {spaces}')
+        frame = Frame('join', kind=Kind.COMMAND, data={'spaces': spaces})
+        await self._transport.send(frame)
+
+    async def leave(self, spaces):
+        if isinstance(spaces, str):
+            spaces = [spaces]
+        else:
+            spaces = list(spaces)
+        logger.warning(f'Leaving spaces {spaces}')
+        frame = Frame('leave', kind=Kind.COMMAND, data={'spaces': spaces})
+        await self._transport.send(frame)
