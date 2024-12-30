73ceed18f6053272a0cf5befc8c8dc3d0fa35a16
Harshad Sharma <harshad@sharma.io>
2024-12-30 22:18:26 +0530

    Format

--- examples/08_random-value.py
+++ examples/08_random-value.py
@@ -1,17 +1,15 @@
 import random
 from zentropi import Agent
 
-a = Agent('random-value')
+a = Agent("random-value")
 
 
-@a.on_interval('random-value', 2)
+@a.on_interval("random-value", 2)
 async def random_value():
-    await a.event(
-        'random-value', 
-        value=random.choice(range(20, 80)))
+    await a.event("random-value", value=random.choice(range(20, 80)))
 
 
 a.run(
-    'ws://localhost:26514/',
-    token='dfa50132fcea4105a69cba3c1424429b',
+    "ws://localhost:26514/",
+    token="test-token",
 )
