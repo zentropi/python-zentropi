072241b6f21fd3992c51eb69b6c5e59c8d195290
Harshad Sharma <harshad@sharma.io>
2024-12-30 22:19:23 +0530

    Fix errors

--- examples/echo_server.py
+++ examples/echo_server.py
@@ -6,18 +6,21 @@ from zentropi import Frame
 from zentropi import Kind
 
 
-async def echo(websocket, path):
+async def echo(websocket):
     async for message in websocket:
         frame = Frame.from_json(message)
-        if frame.kind == Kind.COMMAND and frame.name == 'login':
-            print(f'login-ok', frame.to_dict())
-            await websocket.send(frame.reply('login-ok').to_json())
+        if frame.kind == Kind.COMMAND and frame.name == "login":
+            print(f"login-ok", frame.to_dict())
+            await websocket.send(frame.reply("login-ok").to_json())
         else:
             print(frame.to_dict())
             await websocket.send(message)
 
 
-if __name__ == '__main__':
-    start_server = websockets.serve(echo, "localhost", 26514)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+async def main():
+    server = await websockets.serve(echo, "localhost", 26514)
+    await server.wait_closed()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
